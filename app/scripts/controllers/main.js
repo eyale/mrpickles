c7ad5d5ebe3a8a6f6ef07bd7f24a0da96a457079
Anton Goncharov <antongoncharov@Antons-MacBook-Pro.local>
2015-04-15 14:25:58 +0300

    pubnub controller,module added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,5 +5,11 @@
         .module('mrApp')
         .controller('MainCtrl', MainCtrl);
 
-    function MainCtrl($scope) {}
+    function MainCtrl($scope, PubNub) {
+        PubNub.init({
+            publish_key:'pub key',
+            subscibe_key:'sub key',
+            uuid:'an_optional_user_uuid'
+        })
+    }
 })();
