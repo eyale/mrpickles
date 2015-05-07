65b170a4a68aadc17c66dae3e8d089133d04fadb
Anton Goncharov <antongoncharov@Antons-MacBook-Pro.local>
2015-05-07 15:27:50 +0300

    some improvements

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,7 +6,25 @@
         .controller('MainCtrl', MainCtrl);
 
     function MainCtrl($scope, $firebaseArray) {
-        $scope.massage = $firebaseArray(ref);
         var ref = new Firebase('https://glaring-fire-4469.firebaseio.com/');
+        $scope.messages = $firebaseArray(ref);
+
+        $scope.addMessage = function(e) {
+            if (e.keyCode === 13 && $scope.msg) {
+                var name = $scope.name || 'anonymous';
+                $scope.messages.$add({
+                    from: name,
+                    body: $scope.msg
+                });
+                $scope.msg = '';
+            }
+        };
+        $scope.pipes = $firebaseArray(ref);
+        $scope.addPipeline = function() {
+            var pipeName = $scope.pipeName || 'pipeName';
+            $scope.pipes.$add({
+                from: pipeName
+            });
+        }
     }
 })();
