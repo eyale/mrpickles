5bc16760a4a37bc04e66ea4169794473d38536c8
Anton Goncharov <antongoncharov@Antons-MacBook-Pro.local>
2015-05-13 12:40:02 +0300

    nothing added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,26 +5,6 @@
         .module('mrApp')
         .controller('MainCtrl', MainCtrl);
 
-    function MainCtrl($scope, $firebaseArray) {
-        var ref = new Firebase('https://glaring-fire-4469.firebaseio.com/');
-        $scope.messages = $firebaseArray(ref);
-
-        $scope.addMessage = function(e) {
-            if (e.keyCode === 13 && $scope.msg) {
-                var name = $scope.name || 'anonymous';
-                $scope.messages.$add({
-                    from: name,
-                    body: $scope.msg
-                });
-                $scope.msg = '';
-            }
-        };
-        $scope.pipes = $firebaseArray(ref);
-        $scope.addPipeline = function() {
-            var pipeName = $scope.pipeName || 'pipeName';
-            $scope.pipes.$add({
-                from: pipeName
-            });
-        }
+    function MainCtrl($scope) {
     }
 })();
--- app/settings/pipeline.ctrl.js
+++ app/settings/pipeline.ctrl.js
@@ -1,40 +1,1 @@
-(function() {
-    'use strict';
-    angular.module('mrApp')
-        .controller('PipelineSettingsCtrl', PipelineSettingsCtrl);
-
-    PipelineSettingsCtrl.$inject = ['$scope', '$firebaseArray'];
-
-
-    function PipelineSettingsCtrl($scope, $firebaseArray) {
-        var vm = this;
-        vm.check = 'asd';
-        vm.sel = null;
-
-        var ref = new Firebase('https://glaring-fire-4469.firebaseio.com/pipeline');
-        vm.pipeline = $firebaseArray(ref)
-
-        vm.addStage = addStage;
-
-        vm.addStage = addStage;
-        vm.selEl = selectEl;
-
-
-        init();
-
-        function addStage(title) {
-        	vm.pipeline.$add({
-        		title: title
-        	});
-        }
-
-        function selectEl (el) {
-        	vm.sel = el;
-        }
-
-
-        function init() {
-        }
-    }
-
-})();
+(function() {    'use strict';    angular.module('mrApp')        .controller('PipelineSettingsCtrl', PipelineSettingsCtrl);    PipelineSettingsCtrl.$inject = ['$scope', '$firebaseArray'];    function PipelineSettingsCtrl($scope, $firebaseArray) {        var vm = this;        vm.check = 'asd';        vm.sel = null;        var ref = new Firebase('https://glaring-fire-4469.firebaseio.com/pipeline');        vm.pipeline = $firebaseArray(ref)        vm.addStage = addStage;        vm.selEl = selectEl;        vm.deleteStage = deleteStage;        init();        function deleteStage(index) {            vm.pipeline.$remove(index);        }        function reName() {            debugger;            deleteStage();            addStage();        }        function addStage(title) {            vm.pipeline.$add({                title: title            });        }        function selectEl(el) {            vm.sel = el;        }        function init() {}    }})();
